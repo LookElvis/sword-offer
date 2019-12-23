b2598480885625c5daa1dcd3b9b53ce4a9d4f074
lucian <504866487@qq.com>
2019-12-23 11:16:07 +0800

    update sword 22

--- src/main/java/S21__30/S22.java
+++ src/main/java/S21__30/S22.java
@@ -1,6 +1,7 @@
 package S21__30;
 
 import PublicClass.TreeNode;
+import PublicClass.Utils;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -11,7 +12,18 @@ import java.util.Queue;
  */
 public class S22 {
     public static void main(String[] args) {
-        
+        TreeNode root = new TreeNode(1);
+        TreeNode n1 = new TreeNode(9);
+        TreeNode n2 = new TreeNode(7);
+        TreeNode n3 = new TreeNode(4);
+        TreeNode n4 = new TreeNode(2);
+        root.left = n1;
+        root.right = n2;
+        n2.left = n3;
+//        n2.right = n4;
+        Utils.printTree(root);
+
+        Utils.printArrayList(PrintFromTopToBottom(root));
     }
 
     public static ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
