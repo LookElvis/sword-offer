353db4d8f6efb628eb9b47fa0e2fe58b0e239787
lucian <504866487@qq.com>
2019-12-26 13:44:40 +0800

    update sword 30

--- src/main/java/S21__30/S30.java
+++ src/main/java/S21__30/S30.java
@@ -1,10 +1,21 @@
 package S21__30;
 
 /**
+ * 连续子数组的最大和
  * Created by Elvis on 2019/12/24.
  */
 public class S30 {
     public static void main(String[] args) {
+        int[] arr = new int[] {-1,6,-2,7,-15,1,2,2};
+        System.out.println(FindGreatestSumOfSubArray(arr));
+    }
 
+    public static int FindGreatestSumOfSubArray(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            array[i] += array[i - 1] > 0 ? array[i - 1] : 0;
+            max = Math.max(max, array[i]);
+        }
+        return max;
     }
 }
---
+++ src/main/java/S31_40/S31.java
@@ -0,0 +1,10 @@
+package S31_40;
+
+/**
+ * Created by Elvis on 2019/12/26.
+ */
+public class S31 {
+    public static void main(String[] args) {
+
+    }
+}
