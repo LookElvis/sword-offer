c64a33886c5235df8baade35c108d6219bb7554d
lucian <504866487@qq.com>
2019-12-28 11:38:30 +0800

    update utils

--- src/main/java/PublicClass/Utils.java
+++ src/main/java/PublicClass/Utils.java
@@ -8,6 +8,49 @@ import java.util.Queue;
  * Created by liuxiang on 2018/10/11.
  */
 public class Utils {
+    public static TreeNode createTree1() {
+        TreeNode root = new TreeNode(10);
+        TreeNode n1 = new TreeNode(5);
+        TreeNode n2 = new TreeNode(9);
+        TreeNode n3 = new TreeNode(1);
+        TreeNode n4 = new TreeNode(6);
+        TreeNode n5 = new TreeNode(7);
+        TreeNode n6 = new TreeNode(3);
+        TreeNode n7 = new TreeNode(4);
+        TreeNode n8 = new TreeNode(8);
+        root.left = n1;
+        root.right = n2;
+        n1.left = n3;
+        n1.right = n4;
+        n2.left = n5;
+        n2.right = n6;
+        n3.left = n7;
+        n5.right = n8;
+        Utils.printTree(root);
+        return root;
+    }
+
+    public static TreeNode createTree2() {
+        TreeNode root = new TreeNode(10);
+        TreeNode n1 = new TreeNode(4);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(9);
+        TreeNode n4 = new TreeNode(7);
+        TreeNode n5 = new TreeNode(1);
+        TreeNode n6 = new TreeNode(8);
+        TreeNode n7 = new TreeNode(11);
+        TreeNode n8 = new TreeNode(3);
+        root.left = n1;
+        root.right = n2;
+        n1.left = n3;
+        n2.right = n6;
+        n3.left = n7;
+        n7.left = n4;
+        n5.right = n8;
+        Utils.printTree(root);
+        return root;
+    }
+
     public static void printTree(TreeNode head) {
         System.out.println();
         System.out.println("Binary Tree:");
--- src/main/java/S31_40/S38.java
+++ src/main/java/S31_40/S38.java
@@ -1,11 +1,19 @@
 package S31_40;
 
+import PublicClass.TreeNode;
+import PublicClass.Utils;
+
 /**
  * 
  * Created by Elvis on 2019/12/28.
  */
 public class S38 {
     public static void main(String[] args) {
+        TreeNode head = Utils.createTree1();
+    }
 
+    public static int TreeDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(TreeDepth(root.left), TreeDepth(root.right)) + 1;
     }
 }
