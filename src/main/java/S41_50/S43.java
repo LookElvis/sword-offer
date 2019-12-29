362ebba9661e251b6176bd0fed9e1c959cc5766d
lucian <504866487@qq.com>
2019-12-29 15:14:07 +0800

    update sword 43

--- src/main/java/S41_50/S43.java
+++ src/main/java/S41_50/S43.java
@@ -1,10 +1,18 @@
 package S41_50;
 
 /**
+ * 左旋转字符串
  * Created by Elvis on 2019/12/29.
  */
 public class S43 {
     public static void main(String[] args) {
-
+        System.out.println(LeftRotateString("abcXYZdef", 9));
+    }
+    
+    public static String LeftRotateString(String str,int n) {
+        if (str == null || str.length() == 0) return str;
+        int len = n % str.length();
+        if (len == 0) return str;
+        return new StringBuilder(str.substring(len)).append(str.substring(0, len)).toString();
     }
 }
