4ec381214fa033274970f18dbe21bd9c7e4a9455
lucian <504866487@qq.com>
2019-12-30 11:16:31 +0800

    update sword 47

--- src/main/java/S41_50/S46.java
+++ src/main/java/S41_50/S46.java
@@ -1,6 +1,7 @@
 package S41_50;
 
 /**
+ * 圆圈中最后剩下的数
  * Created by Elvis on 2019/12/30.
  */
 public class S46 {
--- src/main/java/S41_50/S47.java
+++ src/main/java/S41_50/S47.java
@@ -1,10 +1,19 @@
 package S41_50;
 
-/**
+/** 求1+2+3+...+n
  * Created by Elvis on 2019/12/30.
  */
 public class S47 {
     public static void main(String[] args) {
+        System.out.println(Sum_Solution1(4));
+    }
 
+    public static int Sum_Solution1(int n) {
+        return ((1 + n) * n) >> 1;
+    }
+
+    public static int Sum_Solution(int n) {
+        if (n == 1) return 1;
+        return Sum_Solution(n - 1) + n;
     }
 }
