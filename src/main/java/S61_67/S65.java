1c5bd7f0b1d7a38929cf18fba1278c06e9f7da2e
lucian <504866487@qq.com>
2020-01-04 14:15:09 +0800

    update sword 65

--- src/main/java/S61_67/S65.java
+++ src/main/java/S61_67/S65.java
@@ -6,10 +6,41 @@ package S61_67;
  */
 public class S65 {
     public static void main(String[] args) {
-        char[] matrix = new char[]{'a', 'b', 'c', 'e', 's', 'f', 'c', 's', 'a', 'd', 'e', 'e'};
+        String matrix = "abcesfcsadee";
+        String str = "abcb";
+        int rows = 3;
+        int cols = 4;
+        System.out.println(hasPath(matrix.toCharArray(), rows, cols, str.toCharArray()));
     }
 
-//    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
-//
-//    }
+    public static boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+        boolean[] flag = new boolean[matrix.length];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (judge(matrix, i, j, rows, cols, str, 0, flag)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean judge(char[] matrix, int i, int j, int rows, int cols, char[] str, int k, boolean[] flag) {
+        int index = i * cols + j;
+        if (i < 0 || i >= rows || j < 0 || j >= cols || matrix[index] != str[k] || flag[index]) {
+            return false;
+        }
+        if (k == str.length - 1) {
+            return true;
+        }
+        flag[index] = true;
+        if (judge(matrix, i - 1, j, rows, cols, str, k + 1, flag)
+                || judge(matrix, i + 1, j, rows, cols, str, k + 1, flag)
+                || judge(matrix, i, j - 1, rows, cols, str, k + 1, flag)
+                || judge(matrix, i, j + 1, rows, cols, str, k + 1, flag)) {
+            return true;
+        }
+        flag[index] = false;
+        return false;
+    }
 }
