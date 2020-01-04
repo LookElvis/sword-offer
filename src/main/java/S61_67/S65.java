d1d5c25a6a90d1d79b638d76463997ee8bb7c48a
lucian <504866487@qq.com>
2020-01-04 12:30:39 +0800

    update sword 67

--- src/main/java/S61_67/S65.java
+++ src/main/java/S61_67/S65.java
@@ -1,10 +1,15 @@
 package S61_67;
 
 /**
+ * 矩阵中的路径
  * Created by Elvis on 2020/1/4.
  */
 public class S65 {
     public static void main(String[] args) {
-
+        char[] matrix = new char[]{'a', 'b', 'c', 'e', 's', 'f', 'c', 's', 'a', 'd', 'e', 'e'};
     }
+
+//    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+//
+//    }
 }
--- src/main/java/S61_67/S67.java
+++ src/main/java/S61_67/S67.java
@@ -5,6 +5,20 @@ package S61_67;
  */
 public class S67 {
     public static void main(String[] args) {
+        System.out.println(cutRope(2));
+    }
 
+    public static int cutRope(int target) {
+        if (target == 1) return 1;
+        int[] m = new int[target + 1];
+        m[1] = 1;
+        for (int i = 2; i <= target; i++) {
+            int max = m[i];
+            for (int j = 1; j <= i / 2; j++) {
+                max = Math.max(max, Math.max(j, m[j]) * Math.max(i - j, m[i - j]));
+            }
+            m[i] = max;
+        }
+        return m[target];
     }
 }
