f0ec6312fcac48bc21b75875228c284cf96318d7
lucian <504866487@qq.com>
2020-01-06 13:06:22 +0800

    update new solution

--- src/main/java/S1_10/S2.java
+++ src/main/java/S1_10/S2.java
@@ -7,16 +7,47 @@ package S1_10;
 public class S2 {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer("We Are Happy.");
-        System.out.println(replaceSpace(sb));
+        System.out.println(replaceSpace1(sb));
     }
-    public static String replaceSpace(StringBuffer str) {
+
+    public static String replaceSpace2(StringBuffer str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static String replaceSpace1(StringBuffer str) {
         if (str == null || str.length() == 0) return "";
+        int count = 0;
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == ' ') {
-                str.deleteCharAt(i);
-                str.insert(i, "%20");
+                count++;
+            }
+        }
+        int oldLen = str.length();
+        str.setLength(oldLen + 2 * count);
+        for (int i = oldLen - 1; i >= 0; i--) {
+            if (str.charAt(i) != ' ') {
+                str.setCharAt(i + 2 * count, str.charAt(i));
+            } else {
+                str.setCharAt(i + 2 * count, '0');
+                str.setCharAt(i + 2 * count - 1, '2');
+                str.setCharAt(i + 2 * count - 2, '%');
+                count--;
             }
         }
         return str.toString();
+    }
+
+    public static String replaceSpace(StringBuffer str) {
+        if (str == null || str.length() == 0) return "";
+        return str.toString().replace(" ","%20");
     }
 }
