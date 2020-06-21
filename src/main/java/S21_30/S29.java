03bdd9880bf02e1a6e61b768c94db6c5542de3be
liuxiang <504866487@qq.com>
2020-06-21 17:29:15 +0800

    update

--- src/main/java/S21_30/S29.java
+++ src/main/java/S21_30/S29.java
@@ -3,6 +3,9 @@ package S21_30;
 import PublicClass.Utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * 最小的K个数
@@ -11,8 +14,24 @@ import java.util.ArrayList;
 public class S29 {
     public static void main(String[] args) {
         int[] input = new int[] {4,5,1,6,2,7,3,8};
-        ArrayList<Integer> list = GetLeastNumbers_Solution(input, 8);
+        ArrayList<Integer> list = GetLeastNumbers_Solution(input, 4);
         Utils.printArrayList(list);
+    }
+
+    public static List<Integer> GetLeastNumbers_Solution1(int[] array, int k) {
+        List<Integer> res = new ArrayList<>();
+        if (array == null || array.length == 0) {
+            return res;
+        }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < array.length; i++) {
+            priorityQueue.add(array[i]);
+        }
+        for (int i = 0; i < k; i++) {
+            res.add(priorityQueue.poll());
+        }
+        return res;
     }
 
     public static ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
